011d9f0c969e3d645809be7c27c5055fc5af01c0
RoneTds <135079868+RoneTds@users.noreply.github.com>
2023-09-14 21:41:59 -0300

    Aula 140923

---
+++ src/report/report.entity.ts
@@ -0,0 +1,18 @@
+
+import { Entity, PrimaryGenerationColum, Colum} from 'typeorm';
+
+@Entity()
+export class Report {
+    @PrimaryGenerationColum()
+    id: number;
+
+    @Colum()
+    title: string;
+
+    @Colum()
+    description: string;
+
+    @Colum({ default: 'Em Analise'})
+    status: string: // pode ser "Em Analise", "Em Processo" ou "Concluido"
+
+}
